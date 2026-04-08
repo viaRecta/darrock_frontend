14e7de18bb11d0f6b614f2985ed1e6d0b09dbd74
yelizdemirtas <yeliz.demirtas34@hotmail.com>
2026-04-09 00:28:20 +0300

    tasarim
    tasarim

--- tracking/app.py
+++ tracking/app.py
@@ -9,16 +9,44 @@ from api_client import get_json
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def track_portfolio():
+def build_tracking_params():
     params = {
         'portfolio_id': request.args.get('portfolio_id', 20, type=int),
         'year': request.args.get('year', type=int),
         'mode': request.args.get('mode', 'fixed_shares'),
         'initial_cash': request.args.get('initial_cash', 10000, type=float),
     }
+    store_portfolio = request.args.get('store_portfolio')
+    if store_portfolio is not None:
+        params['store_portfolio'] = store_portfolio
+    return params
+
+
+def render_tracking_template(template_name: str):
+    params = build_tracking_params()
     data = get_json('/api/tracking/portfolio', params=params)
-    return render_template('tracking3.html', **data)
+    return render_template(template_name, **data)
+
+
+@app.route('/', methods=['GET'])
+@app.route('/track_portfolio', methods=['GET'])
+def track_portfolio():
+    return render_tracking_template('tracking3.html')
+
+
+@app.route('/legacy', methods=['GET'])
+def track_portfolio_legacy():
+    return render_tracking_template('tracking3eski.html')
+
+
+@app.route('/tracking3eski.html', methods=['GET'])
+def track_portfolio_legacy_file():
+    return render_tracking_template('tracking3eski.html')
+
+
+@app.route('/tracking3.html', methods=['GET'])
+def track_portfolio_current_file():
+    return render_tracking_template('tracking3.html')
 
 
 if __name__ == '__main__':
